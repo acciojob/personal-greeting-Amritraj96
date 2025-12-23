c0d13122cd52ff2538ed97f6b3756eb1b04ad499
Amritraj96 <amritraj.up.yadav@gmail.com>
2025-12-23 23:30:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,13 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      {name && <h2>Hello, {name}!</h2>}
     </div>
   )
 }
